6fd820c81631296d2d775d288096cdc1b6310a3d
Vale the Violet Mote <valethemajor@gmail.com>
2019-12-05 16:31:43 -0500

    full path

--- lib/day/two.ex
+++ lib/day/two.ex
@@ -9,19 +9,19 @@ defmodule Advent.Day.Two do
 
  99 indicates the end of the program.
 
-  iex> process_program [1,9,10,3,2,3,11,0,99,30,40,50]
+  iex> Advent.Day.Two.process_program [1,9,10,3,2,3,11,0,99,30,40,50]
   [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]
 
-  iex> process_program [1,0,0,0,99]
+  iex> Advent.Day.Two.process_program [1,0,0,0,99]
   [2, 0, 0, 0, 99]
 
-  iex> process_program [2,3,0,3,99]
+  iex> Advent.Day.Two.process_program [2,3,0,3,99]
   [2, 3, 0, 6, 99]
 
-  iex> process_program [2,4,4,5,99,0]
+  iex> Advent.Day.Two.process_program [2,4,4,5,99,0]
   [2,4,4,5,99,9801]
 
-  iex> process_program [1,1,1,4,99,5,6,0,99]
+  iex> Advent.Day.Two.process_program [1,1,1,4,99,5,6,0,99]
   [30,1,1,4,2,5,6,0,99]
 
  """
