fed59ddb7ed0cc4206b7c7d94a65879fa9f2854c
Vale the Violet Mote <valethemajor@gmail.com>
2019-12-05 14:30:08 -0500

    part two changes

--- lib/day/one.ex
+++ lib/day/one.ex
@@ -39,12 +39,52 @@ defmodule Advent.Day.One do
   |> Enum.map(fn str -> with {n, _} <- Integer.parse(str), do: n end)
 
 
+
   @doc """
-  Returns the answer of the Day 1 challenge.
+  Revised version of the determine fuel function, now taking into account the
+  need to recurse calculation on fuel. Still uses the old function to prevent DRY.
+    iex> Advent.Day.One.determine_fuel_need_revised 14
+    2
+    iex> Advent.Day.One.determine_fuel_need_revised 1969
+    966
+    iex> Advent.Day.One.determine_fuel_need_revised 100756
+    50346
+  """
+  @spec determine_fuel_need_revised(number, number) :: number
+  def determine_fuel_need_revised(mass, acc \\ 0)
+  def determine_fuel_need_revised(0, acc) when is_number(acc) do
+    acc
+  end
+  def determine_fuel_need_revised(mass, acc) when is_number(mass) do
+    case determine_fuel_need(mass) do
+      new_mass when new_mass > 0  -> determine_fuel_need_revised(new_mass, acc+new_mass)
+      _ -> determine_fuel_need_revised(0, acc)
+    end
+  end
+
+  @doc """
+  Handles a list for the new form of fuel determination.
+  Something of a DRY violation as it does precisely what the previous version minus one change. TODO: marry the two to follow DRY.
+    iex> Advent.Day.One.determine_fuel_need_revised [12, 14, 972167125, 11247751752, 1277512857851728]
+    638762538885064
+  """
+  @spec determine_fuel_need_revised([number], 0) :: number
+  def determine_fuel_need_revised(list_of_masses, 0) when is_list(list_of_masses) do
+    list_of_masses
+    |> Task.async_stream(&determine_fuel_need_revised/1, max_concurrency: System.schedulers_online() * 2)
+    |> Enum.reduce(0, fn {:ok, v}, acc -> acc+v end)
+  end
+
+
+  @doc """
+  Returns the answers of the Day 1 challenge. Default is for part 1
     iex> Advent.Day.One.get_answer
     3318632
+    iex> Advent.Day.One.get_answer 2
+    4975084
   """
-  @spec get_answer :: integer
-  def get_answer, do: determine_fuel_need(@d1data)
-
+  @spec get_answer(part :: number) :: integer
+  def get_answer(part \\ 1)
+  def get_answer(1), do: determine_fuel_need(@d1data)
+  def get_answer(2), do: determine_fuel_need_revised(@d1data)
 end
