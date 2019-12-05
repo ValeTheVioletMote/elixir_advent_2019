a451a4c8e66bee4ba258b59a42954e4ecac24094
Vale the Violet Mote <valethemajor@gmail.com>
2019-12-05 14:48:42 -0500

    spacing and documentation corrections

--- lib/day/one.ex
+++ lib/day/one.ex
@@ -7,12 +7,20 @@ defmodule Advent.Day.One do
 
   @doc """
   Pulls in a mass value and returns the fuel cost.
+
     iex> Advent.Day.One.determine_fuel_need(12)
     2
+
     iex> Advent.Day.One.determine_fuel_need(14)
     2
+
     iex> Advent.Day.One.determine_fuel_need(98273)
     32755
+
+  Can also take in a list of those values and return the sum cost, using tasks for concurrent work.
+
+    iex> Advent.Day.One.determine_fuel_need([12, 14, 972167125, 11247751752, 1277512857851728])
+    425841692590199
   """
   @spec determine_fuel_need(integer) :: integer
   def determine_fuel_need(mass) when is_integer(mass) do
@@ -20,13 +28,6 @@ defmodule Advent.Day.One do
     |> div(3)
     |> (&(&1 - 2)).()
   end
-
-  @doc """
-  Takes a list of mass values and returns the sum cost, using tasks for concurrent work.
-
-    iex> Advent.Day.One.determine_fuel_need([12, 14, 972167125, 11247751752, 1277512857851728])
-    425841692590199
-  """
   @spec determine_fuel_need([integer]) :: integer
   def determine_fuel_need(list_of_masses) when is_list(list_of_masses) do
     list_of_masses
@@ -39,16 +40,25 @@ defmodule Advent.Day.One do
   |> Enum.map(fn str -> with {n, _} <- Integer.parse(str), do: n end)
 
 
-
   @doc """
   Revised version of the determine fuel function, now taking into account the
   need to recurse calculation on fuel. Still uses the old function to prevent DRY.
+
     iex> Advent.Day.One.determine_fuel_need_revised 14
     2
+
     iex> Advent.Day.One.determine_fuel_need_revised 1969
     966
+
     iex> Advent.Day.One.determine_fuel_need_revised 100756
     50346
+
+  Also Handles a list for the new form of fuel determination.
+
+  Something of a DRY violation as it does precisely what the previous version minus one change. TODO: marry the two to follow DRY.
+
+    iex> Advent.Day.One.determine_fuel_need_revised [12, 14, 972167125, 11247751752, 1277512857851728]
+    638762538885064
   """
   @spec determine_fuel_need_revised(number, number) :: number
   def determine_fuel_need_revised(mass, acc \\ 0)
@@ -62,12 +72,6 @@ defmodule Advent.Day.One do
     end
   end
 
-  @doc """
-  Handles a list for the new form of fuel determination.
-  Something of a DRY violation as it does precisely what the previous version minus one change. TODO: marry the two to follow DRY.
-    iex> Advent.Day.One.determine_fuel_need_revised [12, 14, 972167125, 11247751752, 1277512857851728]
-    638762538885064
-  """
   @spec determine_fuel_need_revised([number], 0) :: number
   def determine_fuel_need_revised(list_of_masses, 0) when is_list(list_of_masses) do
     list_of_masses
@@ -78,8 +82,10 @@ defmodule Advent.Day.One do
 
   @doc """
   Returns the answers of the Day 1 challenge. Default is for part 1
+
     iex> Advent.Day.One.get_answer
     3318632
+
     iex> Advent.Day.One.get_answer 2
     4975084
   """
